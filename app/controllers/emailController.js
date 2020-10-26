6d8a38ce28c7327f20c4308d8466ea8af2e8823c
Gabriel <gabrieldiasbnu@hotmail.com>
2020-10-27 00:43:24 +0100

    Email bug / Update packages / script position index.html

--- app/controllers/emailController.js
+++ app/controllers/emailController.js
@@ -1,48 +1,29 @@
 require('dotenv').config();
-const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
   sendVerificationEmail(to, token, userFullName) {
-    const request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: {
-        personalizations: [
-          {
-            to: [
-              {
-                email: to
-              }
-            ],
-            subject: 'Activate Your Email'
-          }
-        ],
-        from: {
-          email: 'noreply@snippetfy-app.herokuapp.com'
-        },
-        content: [
-          {
-            type: 'text/html',
-            value: `
-            <p>
-            Hello ${userFullName}, <br/>
-            Click on this link to verify your email <strong>${
-              process.env.NODE_ENV === 'development'
-                ? 'http://localhost:3000'
-                : 'https://snippetfy-app.herokuapp.com'
-            }/verification?token=${token}&email=${to}</strong><br/>
-            </p>
-
-            `
-          }
-        ]
-      }
-    });
-    return new Promise(function(resolve, reject) {
-      sg.API(request, function(error, response) {
-        if (error) return reject(error);
-        return resolve(response);
-      });
-    });
+    const msg = {
+      to,
+      from: 'test@mydomain.com',
+      subject: 'Activate your email',
+      html: `
+      <p>
+      Hello ${userFullName}, <br/>
+      Click on this link to verify your email <strong>${
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:3333'
+          : 'https://thegabriel.dev/apps/snippetfy'
+      }/verification?token=${token}&email=${to}</strong><br/>
+      </p>`
+    }
+    sgMail.send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
 };
